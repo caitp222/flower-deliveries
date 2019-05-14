912ce08f64a355e95812187a73c46d8f19a4591c
Caitlin Prentke <caitlin.prentke@gmail.com>
2019-05-13 22:23:22 -0400

    make date list unique and clear date from select when clearing filter

--- src/components/deliverables.js
+++ src/components/deliverables.js
@@ -11,6 +11,7 @@ class Deliverables extends Component {
       filteredDeliveries: [],
     }
     this.changeHandler = this.changeHandler.bind(this);
+    this.clearFilter = this.clearFilter.bind(this);
   };
 
   componentDidMount() {
@@ -21,7 +22,7 @@ class Deliverables extends Component {
   componentDidUpdate(prevProps, prevState) {
     if ( prevState.filterDate !== this.state.filterDate ) {
       let filteredDeliveries;
-      if ( this.state.filterDate === "Filter By Date" ) {
+      if ( this.state.filterDate === "Filter By Date" || this.state.filterDate.length === 0 ) {
         filteredDeliveries = [...this.state.allDeliveries];
       } else {
         filteredDeliveries = this.state.allDeliveries.filter((del) =>
@@ -36,20 +37,32 @@ class Deliverables extends Component {
     this.setState({ [e.target.id]: e.target.value })
   }
 
+  clearFilter() {
+    this.setState({ filterDate: "" })
+  }
+
   render() {
-    const { allDeliveries, filteredDeliveries } = this.state;
-    const allDates = allDeliveries.map((del) => del.deliveryDate)
+    const { allDeliveries, filteredDeliveries, filterDate } = this.state;
+
+    let allDates = allDeliveries.map((del) => del.deliveryDate);
+    allDates = allDates.filter(unique);
 
     return(
       <Fragment>
         <FilterMenu
           dates={allDates}
           changeHandler={this.changeHandler}
+          clearFilter={this.clearFilter}
+          filterDate={filterDate}
         />
         <DeliverablesTable deliveries={filteredDeliveries} />
       </Fragment>
     )
   }
+}
+
+const unique = (value, index, self) => {
+  return self.indexOf(value) === index;
 }
 
 export default Deliverables;
--- src/components/filterMenu.js
+++ src/components/filterMenu.js
@@ -1,14 +1,37 @@
 import React, { Fragment } from 'react';
 
 const FilterMenu = (props) => {
-  const { dates, changeHandler } = props;
+  const {
+    dates,
+    changeHandler,
+    clearFilter,
+    filterDate
+  } = props;
 
   return(
     <Fragment>
-      <select className="custom-select" id="filterDate" onChange={changeHandler}>
-        <option defaultValue>Filter By Date</option>
-        { dates.map((date, idx) => <option key={`${date}_${idx}`}>{date}</option>) }
-      </select>
+      <div className="input-group">
+        <select
+          id="filterDate"
+          onChange={changeHandler}
+          className="custom-select"
+          value={filterDate}
+        >
+          <option defaultValue>Filter By Date</option>
+          {
+            dates.map((date, idx) =>
+              <option key={`${date}_${idx}`}>{date}</option>
+            )
+          }
+        </select>
+        <div className="input-group-append">
+          <button
+            className="btn btn-outline-secondary"
+            type="button"
+            onClick={clearFilter}
+          >Clear Filter</button>
+        </div>
+      </div>
     </Fragment>
   )
 };
